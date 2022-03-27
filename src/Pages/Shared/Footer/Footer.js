48dd690a607050e20ca361884eb7fc010d9c4dc7
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-03-27 23:29:02 +0600

    code in footer

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,11 +1,50 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        // <div>
+             <div className=" p-2 bg-dark text-danger">
+  <div className="row">
+    <div className="col">
+        <p>Home</p>
+        <p>Service</p>
+        <p>Contact</p>
+        <p>About CarShop</p>
+    </div>
+    <div className="col">
+            <p>Home</p>
+            <p>Service</p>
+            <p>Contact</p>
+            <p>About CarShop</p>
+    </div>
+    <div className="col">
+        <p>Copyright © 2022 KH IT-Tech</p>
+        <p>We promise to our clients and is <br /> constantlystriving to improve  <br />performance and heightened satisfaction of clients and developer.</p>
+    </div>
+  </div>
+</div>
+        // </div>
     );
 };
 
-export default Footer;
+export default Footer
+
+
+
+{/* <Row className='bg-dark text-danger p-3'>
+<Col>
+    <h4>Barguna Bike Shop</h4>
+    <p>We are providing best BIKES in this town</p>
+</Col>
+<Col xs={5}>
+    
+</Col>
+<Col xs={5}>
+   >
+</Col>
+{/* <Col>
+    
+</Col> */}
+// </Row> */}
